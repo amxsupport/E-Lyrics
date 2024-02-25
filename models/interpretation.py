09387b58140dface0fcf68ac393f166310e2a318
amxsupport <itmasterdesigne@gmail.com>
2024-02-25 16:07:35 +0000

    update interpretation.py implementation of Interpretation class

--- models/interpretation.py
+++ models/interpretation.py
@@ -8,3 +8,15 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import *
 
 
+class Interpretation(BaseModel, Base):
+    """A class named Interpretation
+    Attributes:
+    attr1(text): intepretation text
+    attr2(song_id): song id associated with interpretation
+    attr3(word_id): word id associated with interpretation
+    """
+    __tablename__ = 'interpretations'
+    text = Column(String(600), nullable=False)
+    song_id = Column(String(60), ForeignKey('songs.id'), nullable=False)
+    word_id = Column(String(60), ForeignKey('words.id'), nullable=False)
+    likes = Column(Integer, default=0, nullable=False)
