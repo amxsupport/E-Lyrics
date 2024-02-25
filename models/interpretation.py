29fec040f7b04ffaee6af4046b1864c6e90019af
amxsupport <itmasterdesigne@gmail.com>
2024-02-25 16:02:04 +0000

    create interpretation.py

---
+++ models/interpretation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates the Interpretation class"""
+
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from sqlalchemy import *
+
+
