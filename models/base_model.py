1137b9e7b84221f50a097553a94fede97efc18f0
amxsupport <itmasterdesigne@gmail.com>
2024-02-25 15:28:27 +0000

    create the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -9,4 +9,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+class BaseModel():
+    """A class named BaseModel
+    Attributes:
+    attr1(id): object id
+    attr2(created_at): datetime instance is created
+    attr3(updated_at): datetime instance is created and updated when changed
+    """
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
 
