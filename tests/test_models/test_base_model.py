3e162ed63e453a9c2e8b13fb4e648ffd49dbb4f3
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-26 13:16:48 +0100

    Create test_base_model.py

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,8 @@
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+from unittest.mock import patch
+
+class TestBaseModel(unittest.TestCase):
+    """Test cases for the BaseModel class"""
+
