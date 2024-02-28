72c258f571608937bfea0c6c0b1581508af5822b
amxsupport <itmasterdesigne@gmail.com>
2024-02-28 01:12:53 +0000

    create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This file creates an instance of Storage. It also imports the definitions of
+the BaseModel and Song classes"""
+from models.base_model import BaseModel
+from models.song import Song
+from models.word import Word
+
