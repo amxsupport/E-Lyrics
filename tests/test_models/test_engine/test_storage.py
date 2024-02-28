009537d4819b0f05d49602b4e96bdb496ba5afd8
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-28 14:09:00 +0100

    Create test_storage.py

---
+++ tests/test_models/test_engine/test_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines unnittests for models/engine/storage.py"""
+import unittest
+from models.engine.storage import Storage
+from models.song import Song
+from models.word import Word
+from models.interpretation import Interpretation
+from models.suggestion import Suggestion
+from models.base_model import BaseModel
+import os
+
