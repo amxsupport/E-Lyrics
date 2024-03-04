c64275052fcd2764cd931090d6f05c0e32db8d63
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-03-04 22:51:06 +0100

    Update song_page.js implement displayInterpretations function
    fetches and displays interpretations for word in specific song

--- web_app/static/scripts/song_page.js
+++ web_app/static/scripts/song_page.js
@@ -259,3 +259,29 @@ document.addEventListener('DOMContentLoaded', function (event) {
       console.error(err);
     }}
 
+  /**
+ * Fetches and displays intepretations for word in specific song
+ *
+ * @returns {undefined}
+ */
+  async function displayInterpretations () {
+    try {
+      word = document.getElementById('selectedWord').getAttribute('text');
+      wordIdUrl = 'http://0.0.0.0:5001/api/v1/words/' + word;
+      let wordIdResponse = await fetch(wordIdUrl);
+      let wordIdData = await wordIdResponse.json();
+      let interpretationsApiResponse = await fetch('http://0.0.0.0:5001/api/v1/interpretations/' + wordIdData + '/' + id)
+      let interpretationsData = await interpretationsApiResponse.json()
+      displaySection = setupDisplaySection();
+      document.getElementById('displaySection').insertAdjacentHTML('beforeend', displaySection);
+      contentDiv = document.getElementById('content-div');
+      if (interpretationsData.length > 0) {
+	for (i = 0; i < interpretationsData.length; i++) {
+	  contentDiv.insertAdjacentHTML('beforeend', `<p class="card-text">"<i>${interpretationsData[i].text}</i>"</p>`);
+	}
+      } else {
+	contentDiv.insertAdjacentHTML('beforeend', `<p class="card-text">Be the first to share what you think the artist means by <i>${word}</i></p>`);
+      }} catch(err) {
+	console.error(err);
+      }}
+
