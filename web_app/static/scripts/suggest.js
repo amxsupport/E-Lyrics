cc2bc436dee7e07be9bb261677604cfef570e7f8
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-03-05 23:47:09 +0100

    Update suggest.js implement postSuggestion function
    Gets HTML element values and posts suggestion data to internal REST API

--- web_app/static/scripts/suggest.js
+++ web_app/static/scripts/suggest.js
@@ -13,3 +13,27 @@ document.addEventListener('DOMContentLoaded', function (event) {
     form = document.getElementById('suggestion-form');
     form.addEventListener('submit', postSuggestion);
   }
+  /**
+ * Gets HTML element values and posts suggestion data to internal REST API
+ * @param {event object} event
+ * @returns {undefined}
+ */
+  function postSuggestion (event) {
+    event.preventDefault();
+    suggestedArtist = document.getElementById('artist-text-area').value;
+    suggestedSong = document.getElementById('song-text-area').value;
+    suggestedWords = document.getElementById('words-text-area').value;
+    name = document.getElementById('name-text-area').value;
+    email = document.getElementById('email-text-area').value;
+    suggestionDict = { suggested_artist: suggestedArtist, suggested_song: suggestedSong, suggested_words: suggestedWords, name: name, email: email };
+    suggestionUrl = 'http://0.0.0.0:5001/api/v1/suggestions';
+    fetch(suggestionUrl, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(suggestionDict) })
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById('suggestion-form').style.display = 'none';
+        confirmationDialog = "<br><p style=\"text-align: center;\">Thank you for your suggestion! We'll email you if we add the song!</p>";
+        document.getElementById('suggestion-section').insertAdjacentHTML('beforeend', confirmationDialog);
+      })
+      .catch(error => console.error(error));
+  }
+});
