4d8c79acdf95f96552db026019a8fa19f2b27615
amxsupport <itmasterdesigne@gmail.com>
2024-03-07 16:42:23 +0000

    create __init__.py and views dir

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""creates a flask Blueprint and imports all views which use Blueprint"""
+
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
