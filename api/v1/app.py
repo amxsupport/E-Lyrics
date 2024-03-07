a176e8874127518d6a4f6fc8c150eed02771d9cc
amxsupport <itmasterdesigne@gmail.com>
2024-03-07 16:44:35 +0000

    create the app.py tostarts the API Flask app

---
+++ api/v1/app.py
@@ -0,0 +1,12 @@
+"""starts the Lyrics for Learning API Flask app"""
+
+import os
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from flask import Blueprint
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": 'http://0.0.0.0:5000'}})
