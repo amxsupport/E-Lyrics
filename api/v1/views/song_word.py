15228477669edb3c5fc455a52287d8a8da7e2c57
amxsupport <itmasterdesigne@gmail.com>
2024-03-07 17:00:48 +0000

    update song_word.py implement get_words_for_song

--- api/v1/views/song_word.py
+++ api/v1/views/song_word.py
@@ -8,4 +8,15 @@ from api.v1.views import app_views
 from models.song import Song
 app = Flask(__name__)
 
-
+@app_views.route('/songs/<song_id>/words', methods=['GET'],
+                 strict_slashes=False)
+def get_words_for_song(song_id):
+    """Retrieves all words from a song and returns a list containing
+    all of them"""
+    song = storage.get("Song", song_id)
+    if song is None:
+        return jsonify({}), 404
+    word_list = []
+    for word in song.words:
+        word_list.append(word.to_dict())
+    return jsonify(word_list), 200
