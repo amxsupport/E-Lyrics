e38eb030f0b64cf4e6ab429f767d134efcc7b6d9
amxsupport <itmasterdesigne@gmail.com>
2024-03-07 17:18:16 +0000

    update interpretations.py implement put_interpretation

--- api/v1/views/interpretations.py
+++ api/v1/views/interpretations.py
@@ -11,6 +11,7 @@ from models.interpretation import Interpretation
 from better_profanity import profanity
 app = Flask(__name__)
 
+
 @app_views.route('/interpretations/<word_id>/<song_id>', methods=['GET'],
                  strict_slashes=False)
 def get_interpretations(word_id=None, song_id=None):
@@ -28,6 +29,7 @@ def get_interpretations(word_id=None, song_id=None):
         if interpretation.word_id == word_id and interpretation.song_id == song_id:
             interpretations_list.append(interpretation.to_dict())
     return jsonify(interpretations_list), 200
+
 
 @app_views.route('/interpretations/<word_id>/<song_id>', methods=['POST'],
                  strict_slashes=False)
@@ -55,3 +57,18 @@ def post_interpretation(word_id=None, song_id=None):
     return jsonify(interpretation_obj.to_dict()), 201
 
 
+@app_views.route('/interpretations/<interpretation_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_interpretation(interpretation_id=None):
+    """Updates an Interpretation object"""
+    interpretation_obj = storage.get('Interpretation', interpretation_id)
+    if interpretation_obj is None:
+        abort(404)
+    result = request.get_json()
+    if result is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    if 'likes' not in result:
+        return jsonify({"error": "Missing likes"}), 400
+    setattr(interpretation_obj, "likes", result["likes"])
+    storage.save()
+    return jsonify(interpretation_obj.to_dict()), 200
