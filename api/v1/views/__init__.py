d7e3a1b60846521e267cf87538bae10d715f23ad
amxsupport <itmasterdesigne@gmail.com>
2024-03-07 22:49:11 +0000

    update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,3 +4,8 @@
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.songs import *
+from api.v1.views.song_word import *
+from api.v1.views.interpretations import *
+from api.v1.views.words import *
+from api.v1.views.suggestions import *
+
