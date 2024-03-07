ac85f03150856c4614899b2f19be9b203647ab43
amxsupport <itmasterdesigne@gmail.com>
2024-03-07 16:55:51 +0000

    update song.py api implement get_songs_by_genre def

--- api/v1/views/songs.py
+++ api/v1/views/songs.py
@@ -25,4 +25,12 @@ def get_song(text):
     songs_dict = storage.all(Song)
     return jsonify(songs_dict.get("Song.{:}".format(text)).to_dict()), 200
 
-
+@app_views.route('/songs/genre/<genre>', methods=['GET'], strict_slashes=False)
+def get_songs_by_genre(genre):
+    """Retrieves all Song objects from database with a specified genre"""
+    songs_dict = storage.all(Song)
+    songs_list = []
+    for song in songs_dict.values():
+        if song.genre == genre:
+            songs_list.append(song.to_dict())
+    return jsonify(songs_list), 200
