16a5887fa3de8da20b7d1ecc0e92729d80eb8009
amxsupport <itmasterdesigne@gmail.com>
2024-03-09 12:07:59 +0000

    update add_song.py start Script for testing the addition of songs to the site

--- add_song.py
+++ add_song.py
@@ -9,4 +9,10 @@ from models import storage
 input_artist = input('Artist: ')
 input_song = input('Song: ')
 db_songs = {}
+for song in storage.all(Song).values():
+    db_songs[song.title] = song
+if db_songs.get(input_song) is None:
+
+
+else:
 
